890c75504b5c0f11b01615c82d9da1af330a3406
tolu2507 <tolusnehhil@gmail.com>
2022-12-15 02:43:33 +0100

    my second cond commit on angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './course.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './couses.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
